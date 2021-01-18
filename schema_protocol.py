23a568cdf990b9bec491ea3a4412e5f2c68ba9ad
Marek Cerny# <me@marekcerny.com>
2021-01-18 08:15:22 +0100

    nn-schema protocol init

---
+++ schema_protocol.py
@@ -0,0 +1,29 @@
+from typing import Protocol
+
+
+class NNSchemaProtocol(Protocol):
+
+    __init(self):
+        """ Docs for testing YAPF """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+        pass
