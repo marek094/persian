0286a41207922a0fa9ee6702b5a8f7d25bc75340
Marek Cerny <me@marekcerny.com>
2021-03-14 18:27:03 +0100

    option for experiments

--- persian/main.py
+++ persian/main.py
@@ -5,15 +5,23 @@ from persian.main_grid import dynamic_import
 
 if __name__ == "__main__":
     parser_main = argparse.ArgumentParser()
-    parser_main.add_argument('--schema',
-                             type=str,
-                             default='schema_mnist_cnn.CnnMnistSchema')
-    parser_main.add_argument('--persian', '-p', action='store_true')
+    parser_main.add_argument(
+        '--schema',
+        type=str,
+        default=None,
+        help='for example `schema_mnist_cnn.CnnMnistSchema`')
+    parser_main.add_argument('--persian', type=str, default=None)
+    parser_main.add_argument('--experiments', type=str, default=None)
     args_main, unknown = parser_main.parse_known_args()
 
-    lib = 'persian' if args_main.persian else 'schemas'
+    if args_main.schema is not None:
+        lib, cls_str = 'schemas', args_main.schema
+    elif args_main.persian is not None:
+        lib, cls_str = 'persian', args_main.persian
+    elif args_main.experiments is not None:
+        lib, cls_str = 'experiments', args_main.experiments
 
-    *mds, cls = args_main.schema.split('.')
+    *mds, cls = cls_str.split('.')
     DynSchema = dynamic_import('.'.join([lib] + mds), cls)
 
     parser = DynSchema.build_parser()
