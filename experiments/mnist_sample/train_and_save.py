5bc11164d867c8c536b41e5daa7343a354b6c1cd
Marek Cerny <me@marekcerny.com>
2021-03-14 20:54:17 +0100

    delete obsolete code

--- experiments/mnist_sample/train_and_save.py
+++
@@ -1,9 +0,0 @@
-from persian.trainer import saved_torch_training
-from schemas.schema_mnist_cnn import CnnMnistSchema
-
-if __name__ == "__main__":
-    parser = CnnMnistSchema.build_parser()
-    args = parser.parse_args()
-
-    model = CnnMnistSchema.from_args(args)
-    saved_torch_training(model, verbose=True)
