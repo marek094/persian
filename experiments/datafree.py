f97b6b32a2c2ffd61e1e7141d3ad37a7b370a63c
meu <me@marekcerny.com>
2021-11-06 16:26:52 +0100

    fix scheduler steps

--- experiments/datafree.py
+++ experiments/datafree.py
@@ -1,4 +1,4 @@
-from persian.main import experiment_builder, string_to_args
+from persian.main import experiment_builder, string_to_arggrid
 from persian.trainer import saved_torch_training
 from persian.errors import IncompatibleFlagsError
 
@@ -12,16 +12,20 @@ os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
 
 BEST_EXPERIMENT = """
     --schema prediction_pers
-    --logdir rprs
+    --logdir rprs/2
     --no-ndalg
-    --pers_type l2
+    --pers_type l1;l2
     --no-use_norm
+    --lr 1e-5
+    --lr_inp 1e-2
+    --epochs 100
+    --seed 2021
 """
 
 if __name__ == "__main__":
-    try:
-        model, _ = experiment_builder(
-            default_args=string_to_args(BEST_EXPERIMENT))
-        saved_torch_training(model)
-    except IncompatibleFlagsError as e:
-        print('IncompatibleFlagsError:', e)
+    for args in string_to_arggrid(BEST_EXPERIMENT):
+        try:
+            model, _ = experiment_builder(default_args=args)
+            saved_torch_training(model)
+        except IncompatibleFlagsError as e:
+            print('IncompatibleFlagsError: ', e)
